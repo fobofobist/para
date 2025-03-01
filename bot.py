d3a23a142843cedac5ab3173d46dcde1bc43728c
fobofobist <ahmetcan7korkmaz@gmail.com>
2025-03-01 22:52:44 +0100

    Updated apis

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@ import os
 import tweepy
 
 # Get credentials from environment variables
-API_KEY = os.getenv("API_KEY")
-API_SECRET = os.getenv("API_SECRET")
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-ACCESS_SECRET = os.getenv("ACCESS_SECRET")
+API_KEY = os.getenv("989253d8826582500506eec37e3a7ab2")
+API_SECRET = os.getenv("a1ced770cff6252505d17325c9c54baf")
+ACCESS_TOKEN = os.getenv("0c1e8bac81037bff42488388c8998e2d")
+ACCESS_SECRET = os.getenv("50e844b1de1872b3dd199032b21ffa25")
 
-# Authenticate with Twitter
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+# Authenticate with Twitter AAAAAAAAAAAAAAAAAAAAAFCYzgEAAAAAQuHovv2oU7DuEVkYKlWs%2F6Alv7k%3DUk3u37SNwrSxbcp8b03gCLdWOLJneY7s19zajR8VtIJTff6NvK
+auth = tweepy.OAuthHandler(UTqKtNROvu2BiM6OFlHpCmmxy, 3YvggsZZIsxXnzbNYeAMOjtw5BYJDcUqiSWV8j8S9yn74M9yNj)
+auth.set_access_token(1882142905638268928-GlgM44bDRUvqQxH0BBq2blJm8H4hZd, mW0cY5qlMzSDoY3bLfKu20gwwQmstePGzik4kM077NcaB)
 api = tweepy.API(auth)
 
 # Function to reply to tweets
