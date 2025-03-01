80e55d7b336a1ba0341db3ef983b2df4a1d078d8
fobofobist <ahmetcan7korkmaz@gmail.com>
2025-03-01 23:05:50 +0100

    Fixed API token format

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ ACCESS_SECRET = os.getenv("50e844b1de1872b3dd199032b21ffa25")
 
 # Authenticate with Twitter AAAAAAAAAAAAAAAAAAAAAFCYzgEAAAAAQuHovv2oU7DuEVkYKlWs%2F6Alv7k%3DUk3u37SNwrSxbcp8b03gCLdWOLJneY7s19zajR8VtIJTff6NvK
 auth = tweepy.OAuthHandler("UTqKtNROvu2BiM6OFlHpCmmxy", "3YvggsZZIsxXnzbNYeAMOjtw5BYJDcUqiSWV8j8S9yn74M9yNj")
-auth.set_access_token(1882142905638268928-GlgM44bDRUvqQxH0BBq2blJm8H4hZd, mW0cY5qlMzSDoY3bLfKu20gwwQmstePGzik4kM077NcaB)
+auth.set_access_token("1882142905638268928-GlgM44bDRUvqQxH0BBq2blJm8H4hZd", "mW0cY5qlMzSDoY3bLfKu20gwwQmstePGzik4kM077NcaB")
 api = tweepy.API(auth)
 
 # Function to reply to tweets
